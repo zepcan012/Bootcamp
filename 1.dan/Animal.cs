adf5c5ba770fa8532b393202e34ca704d1ab4293
zepcan012 <lzepcan4@gmail.com>
2022-03-20 20:56:42 +0100

    first app in c#

---
+++ 1.dan/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    public abstract class Animal
+    {
+       public virtual void DisplayText()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ 1.dan/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Dog : Animal, IMessage
+    {
+       
+        public string breed { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine("You chose a dog.");
+        }
+
+        public override void DisplayText()
+        {
+            Console.WriteLine("You added a dog.");
+        }
+
+    }
+}
